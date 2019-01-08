831705430f9dd1e2665cf2dba7d850215f3f93c4
Matt Davidson <mattjdavidson@protonmail.com>
2019-01-08 08:34:17 +0000

    working

--- src/test/java/FibonacciTest.java
+++ src/test/java/FibonacciTest.java
@@ -4,10 +4,4 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class FibonacciTest {
-
-    @Test public void
-    fibonacci_1_1(){
-        assertThat(1, is(1));
-    }
-
 }
